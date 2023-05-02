4ade1a0640a04bd6cb14c6f9a27c82bb074fbb0b
MatthiasLewis <94753137+MatthiasLewis@users.noreply.github.com>
2023-05-02 16:57:21 +0800

    Add files via upload

---
+++ python爬蟲/0502/專題三/auto_to_mysql.py
@@ -0,0 +1,65 @@
+import pymysql
+
+def connectsql(db_name=None,host="localhost",user="admin1",password="123456"):
+    if db_name != None:
+        db = pymysql.connect(host=f"{host}",
+                             user=f"{user}",
+                             password=f"{password}",
+                             database=f"{db_name}")
+        cursor = db.cursor()
+        cursor.execute("show databases like %s",db_name)
+        result = cursor.fetchone()
+        while result is None:
+            db.close()
+            return
+        return db
+    elif db_name == None:
+        db = pymysql.connect(host=f"{host}",
+                             user=f"{user}",
+                             password=f"{password}"
+                             )
+        cursor = db.cursor()
+        return db
+
+def createdatabase(name,fun1):
+    cursor = fun1.cursor()
+    cursor.execute("show databases like %s",name)
+    result = cursor.fetchone()
+    if result is None:
+        cursor.execute(f"create database {name}")
+    else:
+        ans = input(f"名稱重複，即將刪除原本的{name},是否繼續(Y/n)：")
+        if ans == "n" :
+            return
+        elif ans == "Y":
+            cursor.execute(f"drop database {name}")
+            cursor.execute(f"create database {name}")
+
+def createtable(name,fun1,arg):
+    cursor = fun1.cursor()
+    create_t = f"create table {name} "
+    table_set = tuple(arg)
+    create_t = create_t + f"{table_set}".replace("'","")
+    cursor.execute(create_t)
+    fun1.commit()
+    return name
+    
+def inserttable(name,fun1,arg):
+    cursor = fun1.cursor()
+    insert_table = f"insert into {name} values "
+    tuple_table = tuple(arg)
+    insert_table = insert_table + f"{tuple_table}"
+    cursor.execute(insert_table)
+    fun1.commit()
+
+def close(fun1):
+    fun1.close()
+
+def output(name,fun1):
+    cursor = fun1.cursor()
+    cursor.execute(f"select * from {name}")
+    result = cursor.fetchall()
+    for i in result:
+        print(i)
+
+    
---
+++ python爬蟲/0502/專題三/data_to_mysql.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import pickle
+import auto_to_mysql
+
+options = webdriver.ChromeOptions()
+options.add_argument("headless",)
+
+driver = webdriver.Chrome(options = options)
+driver.get("https://gogakuru.com/english/phrase/genre/180_%E5%88%9D%E7%B4%9A%E3%83%AC%E3%83%99%E3%83%AB.html?layoutPhrase=1&orderPhrase=1&condMovie=0&flow=enSearchGenre&condGenre=180&perPage=50")
+
+db = auto_to_mysql.connectsql("test")
+
+table = auto_to_mysql.createtable("exam3",db,["ID int AUTO_INCREMENT" , "content varchar(255)", "PRIMARY KEY(ID)"])
+f1 = open ("exam_3.txt","a")
+f2 = open ("exam_3.pickle","a")
+content1 = driver.find_elements(By.CLASS_NAME, "font-en")
+
+for i in range(len(content1)):
+  content2 = content1[i].text
+  f1.write(content2+"\n")
+  f2.write(content2+"\n")
+  a=i+1
+  auto_to_mysql.inserttable("exam3",db,[a , content2])
+
+f1,f2.close()
+auto_to_mysql.close(db)
+
+
+
+
+
